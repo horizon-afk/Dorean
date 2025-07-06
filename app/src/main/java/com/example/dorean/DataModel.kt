8862da7eb2a001a2cff588377e5298d738fb462b
Umesh <umeshsarkar19@outlook.com>
2025-07-07 01:27:13 +0530

    Made DataModel return games after fetching data

--- app/src/main/java/com/example/dorean/DataModel.kt
+++ app/src/main/java/com/example/dorean/DataModel.kt
@@ -1,13 +1,19 @@
 package com.example.dorean
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-fun requestData() {
+fun requestData(onResult: (List<GameModel>) -> Unit) {
+    var games by mutableStateOf<List<GameModel>>(emptyList())
+    var isLoading by mutableStateOf(true)
+
     val request = APIClient.apiService.getResponse()
     request.enqueue(object : Callback<JsonObject> {
         override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
@@ -15,15 +21,19 @@ fun requestData() {
                 // on success
                 Log.d("LOCATION", "http call successfully")
                 GameModelList.getGameData(response.body()!!)
+                games = GameModelList.games
             } else {
-                // on error
+                games = emptyList()
             }
+            isLoading = false
+            onResult(games)
         }
 
         override fun onFailure(call: Call<JsonObject>, t: Throwable) {
             // on connection error
+            games = emptyList()
+            isLoading = false
+            onResult(games)
         }
-        
     })
-    
 }
