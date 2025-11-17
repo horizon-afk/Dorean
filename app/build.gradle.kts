061ef4c72fa9fd6aa705c933d6dbdb75ed011b04
Umesh <umeshsarkar19@outlook.com>
2025-11-17 06:02:23 +0530

    Updated versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.dorean"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
